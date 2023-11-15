ec99617a075e3e78000a7a182cf0715df00dcf0a
s27dsam <s27dsam@tutanota.com>
2023-11-15 16:50:57 +1100

    deleted main comments

--- main.py
+++ main.py
@@ -2,26 +2,9 @@ from fastapi import FastAPI, Depends, Request, Form, status, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
 from starlette.responses import RedirectResponse
-from database import SessionLocal, engine
-from sqlalchemy.orm import Session
-import models
-from database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 # Mount static files (CSS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -29,48 +12,48 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Jinja2 Templates
 templates = Jinja2Templates(directory="templates")
 
+# Maintain a list to store posts, where each post is a dictionary with title and comments
+posts = []
 
+# Assuming you have a login system in place
 @app.get("/", response_class=HTMLResponse)
 async def login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-
 @app.post("/login")
-async def login(username: str = Form(...), password: str = Form(...)):
-    # Here you should add the logic to validate the username and password
-    if username != "sam" or password != "sam":
-        raise HTTPException(status_code=400, detail="Invalid username or password")
-    return RedirectResponse(url="/main-page", status_code=303)
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    # Add login validation logic here
+    # Redirect to the post list page upon successful login
+    return RedirectResponse(url="/posts", status_code=303)
 
+@app.get("/posts", response_class=HTMLResponse)
+async def posts_list(request: Request):
+    # Display the list of posts
+    return templates.TemplateResponse("posts.html", {"request": request, "posts": posts})
 
-@app.get("/main-page")
-async def main_page(request: Request):
-    # Logic for the main page
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/create-post", response_class=HTMLResponse)
+async def create_post_form(request: Request):
+    # Display the form to create a new post
+    return templates.TemplateResponse("create_post.html", {"request": request})
 
+@app.post("/submit-post")
+async def submit_post(request: Request, title: str = Form(...), post_content: str = Form(...)):
+    # Submit and add the new post to the list of posts
+    post = {"title": title, "content": post_content, "comments": []}
+    posts.append(post)
+    # Redirect to the post list page
+    return RedirectResponse(url="/posts", status_code=303)
 
-@app.get("/post-blog", response_class=HTMLResponse)
-async def post_blog_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.post("/comment/{post_id}")
+async def add_comment(request: Request, post_id: int, comment_content: str = Form(...)):
+    # Add a comment to the specific post
+    if 0 <= post_id < len(posts):
+        posts[post_id]["comments"].append(comment_content)
+    # Redirect back to the post list page
+    return RedirectResponse(url="/posts", status_code=303)
 
-
-@app.post("/post-blog")
-async def post_blog(title: str = Form(...), content: str = Form(...), db: Session = Depends(get_db)):
-    new_blog_post = models.BlogPost(title=title, content=content)
-    db.add(new_blog_post)
-    db.commit()
-    db.refresh(new_blog_post)
-    return RedirectResponse(url="/main-page", status_code=303)
-
-
-def get_all_blog_posts(db: Session):
-    return db.query(models.BlogPost).all()
-
-
-@app.get("/main-page")
-async def main_page(request: Request, db: Session = Depends(get_db)):
-    # Fetch blog posts from the database
-    blog_posts = get_all_blog_posts(db)
-    # Pass them to the template
-    return templates.TemplateResponse("index.html", {"request": request, "blog_posts": blog_posts})
+@app.get("/logout")
+async def logout(request: Request):
+    # Add logout logic here
+    return RedirectResponse(url="/", status_code=303)
 
